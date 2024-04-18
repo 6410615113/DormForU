6a1be685132987a30838b0e9a47b6e90b8ec117b
Pariwat Sawatchat <6410685207@cn103>
2024-04-18 14:40:14 +0700

    Add text and login button in About Us page

--- MyApp/app/src/main/java/com/example/myapp/AboutUs.java
+++ MyApp/app/src/main/java/com/example/myapp/AboutUs.java
@@ -3,6 +3,7 @@ package com.example.myapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +12,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapp.ui.login.LoginActivity;
+
 public class AboutUs extends AppCompatActivity {
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,15 @@ public class AboutUs extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        login = findViewById(R.id.Login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutUs.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
