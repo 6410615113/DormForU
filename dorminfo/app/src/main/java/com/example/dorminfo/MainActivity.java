27a8e5e5402f006535b227c46053543696402f06
Pitchakan Leephud <6410615113@cn103>
2024-04-23 23:36:30 +0700

    complete dorm

---
+++ dorminfo/app/src/main/java/com/example/dorminfo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.dorminfo;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView imageView = (ImageView) findViewById(R.id.campus_map);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Uri uri = Uri.parse("https://maps.app.goo.gl/zdm1RGRTsow95JG2A");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        // link campus
+        TextView campus_facebook = findViewById(R.id.campus_facebook);
+        campus_facebook.setMovementMethod(LinkMovementMethod.getInstance());
+        campus_facebook.setLinkTextColor(Color.BLACK);
+
+        TextView campus_mail = findViewById(R.id.campus_mail);
+        campus_mail.setMovementMethod(LinkMovementMethod.getInstance());
+        campus_mail.setLinkTextColor(Color.BLACK);
+    }
+}
