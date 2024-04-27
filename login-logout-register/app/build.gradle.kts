376200bde24bf2491e883f28c81c624343c0b379
Chanyanuch Jumnongnit <6410615022@student.tu.ac.th>
2024-04-28 01:59:56 +0700

    update favorite but not complete

--- login-logout-register/app/build.gradle.kts
+++ login-logout-register/app/build.gradle.kts
@@ -42,4 +42,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
