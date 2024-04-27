a379831bc15877329398b228df5953b14f5a16e3
T <T@TU>
2024-04-27 15:02:11 +0700

    Dorm Page Ok

--- MyApp/app/src/main/java/com/example/myapp/ui/materials/Dropdown.kt
+++ MyApp/app/src/main/java/com/example/myapp/ui/materials/Dropdown.kt
@@ -1,14 +1,16 @@
 package com.example.myapp.ui.materials
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -26,34 +29,50 @@ import com.example.myapp.ui.ui.theme.MyAppTheme
 @Composable
 fun Dropdown(filter: String = "Area", onFilterChange: (String) -> Unit) {
     val list = listOf("Area", "Price")
-    var selectedText by remember { mutableStateOf(list[0]) }
     var isExpanded by remember {
         mutableStateOf(false)
     }
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            , horizontalAlignment = Alignment.End
+            .width(200.dp)
+            .padding(8.dp), horizontalAlignment = Alignment.End
 
     ) {
-        ExposedDropdownMenuBox(expanded = isExpanded, onExpandedChange = {isExpanded = !isExpanded}) {
-            TextField(modifier = Modifier.menuAnchor(), value = filter, onValueChange = {}, readOnly = true, trailingIcon = {ExposedDropdownMenuDefaults.TrailingIcon(
-                expanded = isExpanded
-            )
-        })
-                    ExposedDropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
-            list.forEachIndexed {index, text ->
-                DropdownMenuItem(
-                    text = { Text(text = text) },
-                    onClick = {
-                        onFilterChange(list[index])
-                        isExpanded = false
-                    },
-                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = !isExpanded }) {
+            val containerColor = Color(0xFF86d4ed)
+            TextField(modifier = Modifier.menuAnchor(),
+
+                value = filter, onValueChange = {},
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = isExpanded
+                    )
+                },
+                shape = RoundedCornerShape(50),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = containerColor,
+                    unfocusedContainerColor = containerColor,
+                    disabledContainerColor = containerColor,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
                 )
+
+            )
+            ExposedDropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
+                list.forEachIndexed { index, text ->
+                    DropdownMenuItem(
+                        text = { Text(text = text) },
+                        onClick = {
+                            onFilterChange(list[index])
+                            isExpanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                    )
+                }
             }
-        }
         }
 
     }
