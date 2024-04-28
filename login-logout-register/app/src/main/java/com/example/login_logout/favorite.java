d4ea3c2ffa1cc1ebc4d9bdddd664ca590a537730
Chanyanuch Jumnongnit <6410615022@cn351>
2024-04-28 22:19:43 +0700

    add activity_favorite

---
+++ login-logout-register/app/src/main/java/com/example/login_logout/favorite.java
@@ -0,0 +1,143 @@
+package com.example.login_logout;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+package com.example.login_logout;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.login_logout.FavAdapter;
+import com.example.login_logout.FavDB;
+import com.example.login_logout.FavItem;
+import com.example.login_logout.R;
+
+import com.example.login_logout.databinding.ActivityFavoriteBinding;
+
+public class favorite extends AppCompatActivity {
+
+    /* private AppBarConfiguration appBarConfiguration;
+    private ActivityFavoriteBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityFavoriteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_favorite);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_favorite);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp(); 
+    }*/
+
+    private RecyclerView recyclerView;
+    private FavDB favDB;
+    private List<FavItem> favItemList = new ArrayList<>();
+    private FavAdapter favAdapter;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        favDB = new FavDB(getActivity());
+        recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // add item touch helper
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView); // set swipe to recyclerview
+
+        loadData();
+
+        return root;
+    }
+
+    private void loadData() {
+        if (favItemList != null) {
+            favItemList.clear();
+        }
+        SQLiteDatabase db = favDB.getReadableDatabase();
+        Cursor cursor = favDB.select_all_favorite_list();
+        try {
+            while (cursor.moveToNext()) {
+                String title = cursor.getString(cursor.getColumnIndex(FavDB.ITEM_TITLE));
+                String id = cursor.getString(cursor.getColumnIndex(FavDB.KEY_ID));
+                int image = Integer.parseInt(cursor.getString(cursor.getColumnIndex(FavDB.ITEM_IMAGE)));
+                FavItem favItem = new FavItem(title, id, image);
+                favItemList.add(favItem);
+            }
+        } finally {
+            if (cursor != null && cursor.isClosed())
+                cursor.close();
+            db.close();
+        }
+
+        favAdapter = new FavAdapter(getActivity(), favItemList);
+
+        recyclerView.setAdapter(favAdapter);
+    }
+
+    // remove item after swipe
+    private ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            final int position = viewHolder.getAdapterPosition(); // get position which is swipe
+            final FavItem favItem = favItemList.get(position);
+            if (direction == ItemTouchHelper.LEFT) { //if swipe left
+                favAdapter.notifyItemRemoved(position); // item removed from recyclerview
+                favItemList.remove(position); //then remove item
+                favDB.remove_fav(favItem.getKey_id()); // remove item from database
+            }
+        }
+    };
+
+}
